4f85b86f28284ea9b1eacec1c27aa74f0b29e1a7
KK <karanjakelvin77@gmail.com>
2022-01-03 05:02:25 +0300

    It will add the jquery for the hover effect

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// This is the click action for the What we do section
 $(document).ready(function(){
     $(".contentsee1").click(function(){
         $(this).toggle(function(){
@@ -34,5 +35,78 @@ $(document).ready(function(){
         $(this).toggle(function(){
             $(".contentsee3").toggle();
         });
+    })
+})
+
+// This is the hover effect for the Portfolio Section
+$(document).ready(function(){
+    $("#port1").mouseenter(function(){
+        $("#hport1").show(1000);
+    })
+    $("#port1").mouseleave(function(){
+        $("#hport1").hide(1000);
+    })
+})
+
+$(document).ready(function(){
+    $("#port2").mouseenter(function(){
+        $("#hport2").show(1000);
+    })
+    $("#port2").mouseleave(function(){
+        $("#hport2").hide(1000);
+    })
+})
+
+$(document).ready(function(){
+    $("#port3").mouseenter(function(){
+        $("#hport3").show(1000);
+    })
+    $("#port3").mouseleave(function(){
+        $("#hport3").hide(1000);
+    })
+})
+
+$(document).ready(function(){
+    $("#port4").mouseenter(function(){
+        $("#hport4").show(1000);
+    })
+    $("#port4").mouseleave(function(){
+        $("#hport4").hide(1000);
+    })
+})
+
+$(document).ready(function(){
+    $("#port5").mouseenter(function(){
+        $("#hport5").show(1000);
+    })
+    $("#port5").mouseleave(function(){
+        $("#hport5").hide(1000);
+    })
+})
+
+$(document).ready(function(){
+    $("#port6").mouseenter(function(){
+        $("#hport6").show(1000);
+    })
+    $("#port6").mouseleave(function(){
+        $("#hport6").hide(1000);
+    })
+})
+
+$(document).ready(function(){
+    $("#port7").mouseenter(function(){
+        $("#hport7").show(1000);
+    })
+    $("#port7").mouseleave(function(){
+        $("#hport7").hide(1000);
+    })
+})
+
+$(document).ready(function(){
+    $("#port8").mouseenter(function(){
+        $("#hport8").show(1000);
+    })
+    $("#port8").mouseleave(function(){
+        $("#hport8").hide(1000);
     })
 })
