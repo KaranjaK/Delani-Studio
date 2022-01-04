ba2aedb504d19bc39a31b76cc4af7908f21eaa59
KK <karanjakelvin77@gmail.com>
2022-01-05 01:07:39 +0300

    It will adds alerts to the form

--- script.js
+++ script.js
@@ -1,112 +1,140 @@
 // This is the click action for the What we do section
-$(document).ready(function(){
-    $(".contentsee1").click(function(){
-        $(this).toggle(function(){
-            $(".contenthide1").toggle();
-        });
-    })
-    $(".contenthide1").click(function(){
-        $(this).toggle(function(){
-            $(".contentsee1").toggle();
-        });
-    })
-})
+$(document).ready(function () {
+  $(".contentsee1").click(function () {
+    $(this).toggle(function () {
+      $(".contenthide1").toggle();
+    });
+  });
+  $(".contenthide1").click(function () {
+    $(this).toggle(function () {
+      $(".contentsee1").toggle();
+    });
+  });
+});
 
-$(document).ready(function(){
-    $(".contentsee2").click(function(){
-        $(this).toggle(function(){
-            $(".contenthide2").toggle();
-        });
-    })
-    $(".contenthide2").click(function(){
-        $(this).toggle(function(){
-            $(".contentsee2").toggle();
-        });
-    })
-})
+$(document).ready(function () {
+  $(".contentsee2").click(function () {
+    $(this).toggle(function () {
+      $(".contenthide2").toggle();
+    });
+  });
+  $(".contenthide2").click(function () {
+    $(this).toggle(function () {
+      $(".contentsee2").toggle();
+    });
+  });
+});
 
-$(document).ready(function(){
-    $(".contentsee3").click(function(){
-        $(this).toggle(function(){
-            $(".contenthide3").toggle();
-        });
-    })
-    $(".contenthide3").click(function(){
-        $(this).toggle(function(){
-            $(".contentsee3").toggle();
-        });
-    })
-})
+$(document).ready(function () {
+  $(".contentsee3").click(function () {
+    $(this).toggle(function () {
+      $(".contenthide3").toggle();
+    });
+  });
+  $(".contenthide3").click(function () {
+    $(this).toggle(function () {
+      $(".contentsee3").toggle();
+    });
+  });
+});
 
 // This is the hover effect for the Portfolio Section
-$(document).ready(function(){
-    $("#port1").mouseenter(function(){
-        $("#hport1").show(1000);
-    })
-    $("#port1").mouseleave(function(){
-        $("#hport1").hide(1000);
-    })
-})
+$(document).ready(function () {
+  $("#port1").mouseenter(function () {
+    $("#hport1").show(1000);
+  });
+  $("#port1").mouseleave(function () {
+    $("#hport1").hide(1000);
+  });
+});
 
-$(document).ready(function(){
-    $("#port2").mouseenter(function(){
-        $("#hport2").show(1000);
-    })
-    $("#port2").mouseleave(function(){
-        $("#hport2").hide(1000);
-    })
-})
+$(document).ready(function () {
+  $("#port2").mouseenter(function () {
+    $("#hport2").show(1000);
+  });
+  $("#port2").mouseleave(function () {
+    $("#hport2").hide(1000);
+  });
+});
 
-$(document).ready(function(){
-    $("#port3").mouseenter(function(){
-        $("#hport3").show(1000);
-    })
-    $("#port3").mouseleave(function(){
-        $("#hport3").hide(1000);
-    })
-})
+$(document).ready(function () {
+  $("#port3").mouseenter(function () {
+    $("#hport3").show(1000);
+  });
+  $("#port3").mouseleave(function () {
+    $("#hport3").hide(1000);
+  });
+});
 
-$(document).ready(function(){
-    $("#port4").mouseenter(function(){
-        $("#hport4").show(1000);
-    })
-    $("#port4").mouseleave(function(){
-        $("#hport4").hide(1000);
-    })
-})
+$(document).ready(function () {
+  $("#port4").mouseenter(function () {
+    $("#hport4").show(1000);
+  });
+  $("#port4").mouseleave(function () {
+    $("#hport4").hide(1000);
+  });
+});
 
-$(document).ready(function(){
-    $("#port5").mouseenter(function(){
-        $("#hport5").show(1000);
-    })
-    $("#port5").mouseleave(function(){
-        $("#hport5").hide(1000);
-    })
-})
+$(document).ready(function () {
+  $("#port5").mouseenter(function () {
+    $("#hport5").show(1000);
+  });
+  $("#port5").mouseleave(function () {
+    $("#hport5").hide(1000);
+  });
+});
 
-$(document).ready(function(){
-    $("#port6").mouseenter(function(){
-        $("#hport6").show(1000);
-    })
-    $("#port6").mouseleave(function(){
-        $("#hport6").hide(1000);
-    })
-})
+$(document).ready(function () {
+  $("#port6").mouseenter(function () {
+    $("#hport6").show(1000);
+  });
+  $("#port6").mouseleave(function () {
+    $("#hport6").hide(1000);
+  });
+});
 
-$(document).ready(function(){
-    $("#port7").mouseenter(function(){
-        $("#hport7").show(1000);
-    })
-    $("#port7").mouseleave(function(){
-        $("#hport7").hide(1000);
-    })
-})
+$(document).ready(function () {
+  $("#port7").mouseenter(function () {
+    $("#hport7").show(1000);
+  });
+  $("#port7").mouseleave(function () {
+    $("#hport7").hide(1000);
+  });
+});
 
-$(document).ready(function(){
-    $("#port8").mouseenter(function(){
-        $("#hport8").show(1000);
-    })
-    $("#port8").mouseleave(function(){
-        $("#hport8").hide(1000);
-    })
-})
+$(document).ready(function () {
+  $("#port8").mouseenter(function () {
+    $("#hport8").show(1000);
+  });
+  $("#port8").mouseleave(function () {
+    $("#hport8").hide(1000);
+  });
+});
+
+// This section will pick data from the form and return a message
+$(document).ready(function () {
+  $("#submit").click(function () {
+    validateData();
+  });
+  function validateData() {
+    let getName = $("#name").val();
+    let getEmail = $("#email").val();
+    let getText = $("#message").val();
+    $(".contactus").submit(function () {
+      if (getName == "") {
+        alert("Kindly provide your name");
+      } else if (getEmail == "") {
+        alert("Kindly provide a valid email");
+        $("#email").focus();
+      } else if (getText == "") {
+        alert("Kindly provide your message");
+      } else {
+        alert(
+          "Thank you " +
+            getName +
+            ". We have successfully received your feedback and are acting upon it."
+        );
+      }
+    });
+  }
+});
